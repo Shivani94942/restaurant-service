e2dd84668a9525b2d8ac4876ae44491adab0425f
Shivani94942 <shivanivv1@gmail.com>
2020-04-30 09:34:50 -0700

    commit

--- src/main/java/com/example/demo/dao/RestaurantDao.java
+++ src/main/java/com/example/demo/dao/RestaurantDao.java
@@ -1,7 +1,5 @@
 package com.example.demo.dao;
 
-import java.util.Optional;
-
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +8,5 @@ import com.example.demo.model.Restaurant;
 @Repository
 public interface RestaurantDao extends CrudRepository<Restaurant, Integer>
 {	
-	public Optional<Restaurant> getRestaurantById(Integer id);
+	
 }
